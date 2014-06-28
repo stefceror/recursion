a0371417111089c86912da6df0db88492c3cb6c3
Stefanie Contreras <stefsnow@gmail.com>
2014-06-28 12:10:55 -0700

    Stringify arrays and distinguishes non-array objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,7 +4,23 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
-
+	var strJSON ="";
+	if(obj){
+		if(obj.constructor ==Array){
+			strJSON = strJSON + "[";
+			for (var i = 0; i < obj.length; i++){
+				strJSON = strJSON + stringifyJSON(obj[i]);
+				if(i<obj.length-1){
+					strJSON = strJSON + ",";
+				}
+			}
+			strJSON = strJSON + "]";
+			return strJSON;
+		} else if (typeof obj ==='object'){
+			return "object";
+		
+		}
+	}
 	if (typeof obj === 'string'){
 		return "\"" + obj.replace(/"/g, "\\$&") + "\""
 	} else {
