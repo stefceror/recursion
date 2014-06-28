081b4459f614a1e6ba5980182006ae11c7ae7531
Stefanie Contreras <stefsnow@gmail.com>
2014-06-28 11:41:53 -0700

    stringifies primitives

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,12 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+
+	if (typeof obj === 'string'){
+		return "\"" + obj.replace(/"/g, "\\$&") + "\""
+	} else {
+		return String(obj);
+	}
+
+  
 };
