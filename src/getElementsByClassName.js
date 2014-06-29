bcde981174eecf950ad369782c81076c717ef076
Stefanie Contreras <stefsnow@gmail.com>
2014-06-28 21:33:47 -0700

    Solved getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,22 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+	var results = [];
+	var getElements = function(className, node) {
+		var innerResults=[];	
+	
+		var pattern = new RegExp("(^|\\s)" + className + "(\\s|$)");
+		if (pattern.test(node.classList)) {
+			innerResults.push(node);
+		}
+		if(node instanceof Element) {
+			for(var i = 0; i < node.childNodes.length; i++) {
+				innerResults = innerResults.concat(getElements(className, node.childNodes[i]));
+			}
+		}
+		return innerResults;
+	}
+	results = getElements(className, document.body);
+
+    return results;
 };
